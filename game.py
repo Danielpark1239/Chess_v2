e6205fa0bcffbe7abceb2bd1372bdf29fd419a3f
Daniel Park <Danielpark1239@gmail.com>
2022-07-20 15:46:26 -0400

    2P mode working, but lots of duplicated code

--- game.py
+++ game.py
@@ -61,7 +61,7 @@ class Game:
 
     # given a psuedo-legal player move, generate the move, displaying
     # a promotion menu if necessary.
-    def generatePlayerMove(self,startCoords, endCoords):
+    def generatePlayerMove(self, startCoords, endCoords):
         promotionMove = chess.Move.from_uci(startCoords+endCoords+"q")
 
         # if the promotion is legal, display the menu and get player input
@@ -103,6 +103,52 @@ class Game:
 
         # if the move is not a promotion, generate a normal UCI
         return chess.Move.from_uci(startCoords + endCoords)
+
+    # Check if the game terminated
+    def checkTermination(self):
+        outcome = self.board.outcome()
+        if outcome is not None:
+            winner = outcome.winner
+            if (winner is False):
+                print("Black won the game!")
+            elif (winner is True):
+                print("White won the game!")
+            elif (winner is None):
+                print("The game ended in a draw!")
+            self.playing = False
+            self.running = False # For now: exits to menu once game terminates
+
+# Run the game against a computer
+class OnePlayer(Game):
+    def __init__(self, difficulty, FENstring, startColor):
+        super().__init__(FENstring=FENstring)
+        pg.display.set_caption("1p Chess Game")
+        self.running = True
+        self.playing = True
+        self.player = startColor # True if the player is white, False for black
+        if (self.player):
+            self.turn = True # True if it's the player's turn
+        else:
+            self.turn = False # False if it's the computer's turn
+            self.displayBoard.flipBoard()
+
+        self.engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+        self.engine.configure({
+            "UCI_LimitStrength": True,
+            "UCI_Elo": difficulty
+        })
+        self.difficulty = difficulty
+        self.debug = ONE_PLAYER_DEBUG # if true, computer plays random moves
+
+        self.gui.menu.add.button(title="Main menu", action=pgm.events.EXIT)
+    
+    def resetGame(self):
+        self.playing = False
+    
+    def resetToMenu(self):
+        self.playing = False
+        self.running = False
+        self.gui.menu.disable()
     
     def checkEvents(self):
         # Computer turn
@@ -126,8 +172,8 @@ class Game:
                 self.running = False
                 pg.quit()
 
-            # while testing: keydown flips the board
-            if event.type == pg.KEYDOWN:
+            # Spacebar flips the board
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 self.displayBoard.flipBoard()
             
             # can be used to: pick up a piece, place down a piece at a diff sq,
@@ -181,57 +227,10 @@ class Game:
                     x, y = pg.mouse.get_pos()
                     self.heldPiece.updateRect(x, y)
 
-    # Check if the game terminated
-    def checkTermination(self):
-        outcome = self.board.outcome()
-        if outcome is not None:
-            winner = outcome.winner
-            if (winner is False):
-                print("Black won the game!")
-            elif (winner is True):
-                print("White won the game!")
-            elif (winner is None):
-                print("The game ended in a draw!")
-            self.playing = False
-            self.running = False # For now: exits to menu once game terminates
-
-# Run the game against a computer
-class OnePlayer(Game):
-    def __init__(self, difficulty, FENstring, startColor):
-        super().__init__(FENstring=FENstring)
-        pg.display.set_caption("1p Chess Game")
-        self.running = True
-        self.playing = True
-        self.player = startColor # True if the player is white, False for black
-        if (self.player):
-            self.turn = True # True if it's the player's turn
-        else:
-            self.turn = False # False if it's the computer's turn
-            self.displayBoard.flipBoard()
-
-        self.engine = chess.engine.SimpleEngine.popen_uci("stockfish")
-        self.engine.configure({
-            "UCI_LimitStrength": True,
-            "UCI_Elo": difficulty
-        })
-        self.difficulty = difficulty
-        self.debug = ONE_PLAYER_DEBUG # if true, computer plays random moves
-
-        self.gui.menu.add.button(title="Main menu", action=pgm.events.EXIT)
-    
-    def resetGame(self):
-        self.playing = False
-    
-    def resetToMenu(self):
-        self.playing = False
-        self.running = False
-        self.gui.menu.disable()
-
     # game loop
     def runGame(self):
         while self.running:
             
-
             while self.playing:
                 # check events
                 self.checkEvents()
@@ -264,6 +263,124 @@ class OnePlayer(Game):
 
                 pg.display.update()
                 self.clock.tick(FPS)
+
+# Run the game against another player (local)
+class TwoPlayer(Game):
+    def __init__(self, FENstring):
+        super().__init__(FENstring=FENstring)
+        pg.display.set_caption("2p Chess Game")
+        self.running = True
+        self.playing = True
+        self.turn = True # True if white to move, False if black to move
+
+        self.gui.menu.add.button(title="Main menu", action=pgm.events.EXIT)
+    
+    def resetGame(self):
+        self.playing = False
+    
+    def resetToMenu(self):
+        self.playing = False
+        self.running = False
+        self.gui.menu.disable()
+    
+    def checkEvents(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.running = False
+                pg.quit()
+
+            # Spacebar flips the board
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+                self.displayBoard.flipBoard()
+            
+            # can be used to: pick up a piece, place down a piece at a diff sq,
+            # place down a piece at the same square (if invalid),
+            # interact with menus if outside the board
+            if event.type == pg.MOUSEBUTTONDOWN:
+                x, y = pg.mouse.get_pos()
+                # mouse is inside the board
+                if (x >= SQUARE_LENGTHS[0] and 
+                    x <= SQUARE_LENGTHS[7] + SQUARE_LENGTH):
+                    squareIndices = self.getSquareIndices(x, y)
+                    
+                    # pick up a piece if none held
+                    if self.heldPiece == None:
+                        # piece exists
+                        if squareIndices in self.displayBoard.pieceMap:
+                            piece = self.displayBoard.pieceMap[squareIndices]
+                            # it's your turn and your color piece
+                            if piece.isWhite() == self.turn:
+                                self.heldPiece = piece
+                                self.heldPieceX = squareIndices[0]
+                                self.heldPieceY = squareIndices[1]
+                    
+                    # if a piece is held, either:
+                    else:
+                        startCoords = self.displayBoard.indicesToStr(
+                            self.heldPieceX, self.heldPieceY)
+                        endCoords = self.displayBoard.indicesToStr(
+                            squareIndices[0], squareIndices[1])
+
+                        # if the squares are distinct, 
+                        if startCoords != endCoords:
+                            # generate a move and play it if it's valid
+                            move = self.generatePlayerMove(startCoords, endCoords)
+                            if move in self.board.legal_moves:
+                                self.heldPiece = None
+                                self.heldPieceX = None
+                                self.heldPiceY = None
+                                self.playMove(move)
+                                self.turn = not self.turn
+                            else:
+                                self.returnPiece()
+
+                        # put down the piece otherwise
+                        else:
+                            self.returnPiece()
+            
+            # if a piece is held, it moves with the mouse cursor
+            if event.type == pg.MOUSEMOTION:
+                if self.heldPiece is not None:
+                    x, y = pg.mouse.get_pos()
+                    self.heldPiece.updateRect(x, y)
+
+    # game loop
+    def runGame(self):
+        while self.running:
+            
+            while self.playing:
+                # check events
+                self.checkEvents()
+
+                # check termination
+                self.checkTermination()
+
+                # draw background
+                self.window.fill((32, 32, 32))
+
+                # draw board
+                for i in range(8):
+                    for j in range(8):
+                        if i % 2 == j % 2:
+                            pg.draw.rect(self.window, 
+                                        LIGHT_GRAY, 
+                                        self.displayBoard.rects[i][j])
+                        else:
+                            pg.draw.rect(self.window,
+                                        LIGHT_BLUE,
+                                        self.displayBoard.rects[i][j])
+            
+                # draw piece sprites
+                pieceList = list(self.displayBoard.pieceMap.values())
+                boardPieces = pg.sprite.Group(pieceList)
+                boardPieces.draw(self.window)
+
+                # update GUI
+                self.gui.menu.draw(self.window)
+
+                pg.display.update()
+                self.clock.tick(FPS)
+
 
 
 # TO-DO:
